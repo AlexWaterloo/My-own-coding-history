748fa9fa427ee40136137c2ad88fd322f6320269
lenovo <zqwaterloo@gmail.com>
2019-01-26 20:35:25 -0500

    nothing

--- 计算考试成绩.py
+++ 计算考试成绩.py
@@ -4,6 +4,6 @@ def final_grade(mid,mtotal,exam,xtotal):
     g2 =exam/xtotal*100
     print(g2)
     f_grade=g1*0.5+g2*0.5
-    # print(f_grade)
+    print(f_grade)
     return f_grade
 final_grade(30,35,80,83)
