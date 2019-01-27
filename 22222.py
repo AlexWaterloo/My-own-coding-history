a6081ed041236229431d580030a74d563b8d986f
lenovo <zqwaterloo@gmail.com>
2019-01-26 20:34:03 -0500

    nothing

---
+++ 22222.py
@@ -0,0 +1,9 @@
+def final_grade(mid,mtotal,exam,xtotal):
+    g1 =mid/mtotal*100
+    print(g1)
+    g2 =exam/xtotal*100
+    print(g2)
+    f_grade=g1*0.5+g2*0.5
+    # print(f_grade)
+    return f_grade
+final_grade(30,35,80,83)
