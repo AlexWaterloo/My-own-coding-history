7879c95c5c73b51291f33d08f6952c4ed56551ef
lenovo <zqwaterloo@gmail.com>
2019-01-31 11:03:00 -0500

    version1

---
+++ UW课程/Lecture_8.py
@@ -0,0 +1,41 @@
+def build_str(los):
+    if los==[]:
+        return ''
+    else:
+        return los[0]+build_str(los[1:])
+print(build_str(['i','hate','spaces']))
+
+print(str.join(' ',['i','hate','spaces']))
+print( 'sdsadfsaf'.upper())
+
+def count_stuff(L):
+    if L==[]:
+        return 0
+    else:
+        return(len(L[0]))+count_stuff(L[1:])
+print(count_stuff(['a','abc','','d']))
+print(3 in [4,3,6,7])
+print('mom'in 'mommy')
+print('mom'in['mommy'])
+print('mom'in['mom'])
+print(2in[2])
+print(2in[4,[2],3])
+print(sum([4,1,6]))
+print([1]*5)
+print([1,4]*5)
+
+
+def weird_sum(M):
+    if M==[]:
+        return 0
+    elif M[0]%2==0:
+        return (sum(M))+weird_sum(M[1:])
+    else:
+        return weird_sum(M[1:])
+print(weird_sum([5,4,3,2,1]))
+
+
+
+print([3,4]+['bb',':'])
+
+a=['world']
---
+++ UW课程/输入input.py
@@ -0,0 +1,8 @@
+
+
+s=input('enter name: ')
+print(s)
+x=input('pick a number:')
+y=input('pick another number')
+total=int(x)+int(y)
+print('together they add up to'+str(total))
